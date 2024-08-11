3525dfd94230a16e3928918d01101854239da1f7
Jessie Jalca <jessiejalca@gmail.com>
2024-08-11 15:37:52 -0400

    style: Begin design revamp to better suit features
    REASON
    After brainstorming and sketching designs, I decided that a different
    layout made more sense. This commit starts to set up the foundation.
    
    CHANGES
    - Simplify UI to focus on Bible page first
    - Get rid of header
    - Add a notes section
    - Simplify virtual DOM

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
 // import { useState } from "react"
-import Header from "./components/Header"
-import Bible from "./components/Bible"
-import "./App.css"
+import Bible from "./components/Bible";
+import "./App.css";
 
 function App() {
   return (
-    <div id="App">
-      <Header />
-      <div className="window">
-        <div className="notes"></div>
-        <Bible />
-      </div>
-    </div>
-  )
+    <>
+      <Bible />
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <header>
-        <input className="bible-tab" placeholder="Search" />
-      </header>
-    )
-}
-
-export default Header
